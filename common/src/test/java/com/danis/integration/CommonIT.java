e3673766478e9af594ddcbd33cdef4f8b6d95c10
Денис <anisimovets@mail.ru>
2023-06-17 12:39:29 +0300

    homework01-maven

---
+++ common/src/test/java/com/danis/integration/CommonIT.java
@@ -0,0 +1,10 @@
+package com.danis.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class CommonIT {
+    @Test
+    public void shouldBeTrue() {
+        assert true;
+    }
+}
---
+++ service/src/test/java/ServiceTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class ServiceTest {
+    @Test
+    public void shouldBeTrue() {
+        assert true;
+    }
+}
